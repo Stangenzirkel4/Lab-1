903b3bb78c1ed93fb1aa03da430677eaebac7c09
Stangenzirkel4 <113057495+Stangenzirkel4@users.noreply.github.com>
2022-09-19 15:40:25 +0300

    Update Lab-1.py

--- Lab-1.py
+++ Lab-1.py
@@ -6,6 +6,9 @@ import csv
 count30 = 0     #Счетчик всех записей
 countall = 0    #Счетчик всех записей с названием > 30 символов
 
+print("Введите искомого автора")
+author = input()    #Имя искомого автора
+
 #Открываем на чтение
 with open('books.csv') as f:
     reader = csv.DictReader(f, delimiter=';')
@@ -18,6 +21,10 @@ with open('books.csv') as f:
         if len(row.get("Название")) > 30:
             count30 += 1
 
+        #Выполняем поиск по автору
+        if author == row.get("Автор"):
+            if int(row.get("Дата поступления")[:4]) >= 2018:
+                print(row)
+
 print("Всего записей:", countall)
 print("Всего записей, у которых в поле 'Название' строка длиннее 30 символов:", count30)
-
