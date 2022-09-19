51426bf5c033e55adcbb2e17cd31f81427b4dfdc
Stangenzirkel4 <113057495+Stangenzirkel4@users.noreply.github.com>
2022-09-19 16:19:48 +0300

    Added user's search
    Реализован генератор библиографических ссылок по запросу пользователя и их вывод в файл

--- Lab-1.py
+++ Lab-1.py
@@ -1,13 +1,21 @@
 #Импортируем необходимое
 import csv
+import random
 
 
 #Объявляем переменные
 count30 = 0     #Счетчик всех записей
 countall = 0    #Счетчик всех записей с названием > 30 символов
+choice = [] #Список для записи выборки по запросу пользователя
 
 print("Введите искомого автора")
 author = input()    #Имя искомого автора
+
+print("Введите параметр поиска (или '-', если хотите получить случайную выборку")
+wantedKey = input()
+if wantedKey != "-":
+    print("Введите значение параметра поиска")
+    wantedValue = input()
 
 #Открываем на чтение
 with open('books.csv') as f:
@@ -26,5 +34,24 @@ with open('books.csv') as f:
             if int(row.get("Дата поступления")[:4]) >= 2018:
                 print(row)
 
+        #Пополняем выборку по запросу пользователя
+        if len(choice) < 20:
+            if wantedKey != "-":
+                if row.get(wantedKey) == wantedValue:
+                    choice.append(row.get("Автор") + '. "' + row.get("Название") + '" - ' + row.get("Дата поступления")[:4])
+            else:
+                if int(row.get("ID")) % 100 == random.randint(0, 99):
+                    choice.append(row.get("Автор") + '. "' + row.get("Название") + '" - ' + row.get("Дата поступления")[:4])
+
+
 print("Всего записей:", countall)
 print("Всего записей, у которых в поле 'Название' строка длиннее 30 символов:", count30)
+
+#Сохраняем 20 книг по запросу
+file = open("User's choice.txt", "w")
+
+n = 0 #Номер строки в записываемом файле
+for i in choice:
+    n += 1
+    file.write(str(n)+". "+str(i)+"\n")
+file.close()
